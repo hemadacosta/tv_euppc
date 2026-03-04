e036036a51290edb562f1f0f5e27869928ae00d0
hemadacosta <156354202+hemadacosta@users.noreply.github.com>
2026-03-04 17:30:46 -0300

    Add files via upload

---
+++ config.js
@@ -0,0 +1,41 @@
+// config.js
+// Lista de vídeos da programação
+// Tipos suportados: 'youtube', 'vimeo', 'googledrive'
+
+const schedule = [
+    {
+        title: "3ª Aula - EUPPC - Por que devemos desinstalar a escola?",
+    url: "https://youtu.be/_Jj7tUgrFbA?si=FpbpRUnrSzk48n3f",
+    type: "youtube"
+    }, 
+    {
+        title: "4ª Aula - EUPPC - Introdução ao documentário Schooling the World",
+    url: "https://youtu.be/wgpRePgjJUA?si=c9ZFsb4LvbhKmezc",
+    type: "youtube"
+    },
+    {
+        title: "Escolarizando o Mundo [Schooling the World]",
+    url: "https://youtu.be/6t_HN95-Urs?si=vMepQ1Xq1hy7XRai",
+    type: "youtube"
+    },
+    {
+        title: "Vermelho Brasil",
+        url: "https://youtu.be/6IAcEAV7hAM?si=bwV42d3bOfz1OZJb",
+        type: "youtube"
+    },
+    {
+        title: "Entrevista com o pensador Ivan Illich",
+        url: "https://youtu.be/feHCNEdLSDE?si=wIRZkPAMhTkfZKmy",
+        type: "youtube"
+    },
+    {
+        title: "Um Triunfo na América - A Epopeia dos Sete Povos das Missões",
+        url: "https://youtu.be/6P6S71cSzYc?si=3vYOjCtVl4tClNrD",
+        type: "youtube"
+    },  
+    {
+        title: "Como corrupção de políticos devastou e impactou a perspectiva futura de Angola",
+        url: "https://youtu.be/LORo4bP5uMo?si=ztlBBZJGvBPgCwTl",
+        type: "youtube"
+    }, 
+        ];
