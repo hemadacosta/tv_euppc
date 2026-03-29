84e387824f7403b519166268d5cb43e50659c4ed
hemadacosta <156354202+hemadacosta@users.noreply.github.com>
2026-03-29 14:23:10 -0300

    Update video schedule in config.js
    Signed-off-by: hemadacosta <156354202+hemadacosta@users.noreply.github.com>

--- config.js
+++ config.js
@@ -4,53 +4,78 @@
 
 const schedule = [
 {
-        title: "3ª Videoaula - EUPPC - Por que devemos desinstalar a escola",
-        url: "https://youtu.be/_Jj7tUgrFbA?si=KxPDpfuTWYlcq_Ah",
+        title: "5ª Aula - EUPPC - Reflexões sobre o documentário Schooling the World",
+        url: "https://youtu.be/ocKkkbSAl4g?si=sDDTCXTo2wLSgJsM",
         type: "youtube"
     },
 {
-        title: "4ª Videoaula - EUPPC - Introdução ao documentário Schooling the World",
-        url: "https://youtu.be/wgpRePgjJUA?si=4XmTh4XfNjgBlVj7",
+        title: "6ª Aula - EUPPC  - Diferenças no processo educacional colonial a partir da catequese operadas por diferentes ordens religiosas",
+        url: "https://youtu.be/2w0IWQP2WLA?si=AmluB6s5wr3bajN9",
         type: "youtube"
     },
 {
-        title: "Schooling the World (Escolarizando o Mundo), o último fardo do homem branco",
-        url: "https://youtu.be/6t_HN95-Urs?si=J8XEwF_pH_51lqiB",
+        title: "Anna de o Rei (1999), Trailer (Assista ao filme completo na apresentação da 5ª Aula - EUPPC",
+        url: "https://youtu.be/I6cF4_502aw?si=NWE5szdtOKzpi10p",
         type: "youtube"
     },
 {
-        title: "Capitão Fantástico (2016)",
-        url: "https://youtu.be/hkzW4ft8P0k?si=-vM-lQuRdcvoH8tZ",
+        title: "Timor-Leste, O Massacre que o Mundo Não Viu (2001)",
+        url: "https://youtu.be/jBx6OMtJ78o?si=fr4Qq4j-qNb7SEYN",
         type: "youtube"
     },
 {
-        title: "Entrevista com o pensador Ivan Illich",
-        url: "https://youtu.be/feHCNEdLSDE?si=wIRZkPAMhTkfZKmy",
+        title: "História de Timor-Leste",
+        url: "https://youtu.be/fD6Clm6teME?si=ASZyvHN0KJgsBk7M",
         type: "youtube"
     },
 {
-        title: "Como corrupção de políticos devastou e impactou a perspectiva futura de Angola",
-        url: "https://youtu.be/LORo4bP5uMo?si=ztlBBZJGvBPgCwTl",
+        title: "Luta Diplomasia Maun Bot Sira Nian Iha Mundo Internasional Defende Povo Timor Nia Direito 1",
+        url: "https://youtu.be/M77VBauUeDg?si=if_zGhHQXiSeQ_8w",
+        type: "youtube"
+    },
+{
+        title: "Brasil e Timor-Leste - 20 anos de amizade e cooperação",
+        url: "https://youtu.be/MT6OVyXcU-c?si=G9A7cYt5Eef8JMfT",
         type: "youtube"
     }, 
 {
-        title: "Arte Educação | SESC-MT - A colonialidade da arte - Módulo I",
-        url: "https://www.youtube.com/embed/ojyLi3cLt9k?si=UbBAMH5lR-Ey6T3v",
+        title: "Arte Educação, SESC-MT - A colonialidade da arte - Módulo II",
+        url: "https://www.youtube.com/embed/SBYwguIUY5s?si=Y18z_c97D0-YqNK7",
         type: "youtube"
     },
 {
-        title: "O Povo Brasileiro de Darcy Ribeiro - Matriz Lusa (parte 2)",
-        url: "https://youtu.be/ZxOCLwMQ_ik?si=r3CT8fNKtYfvscNI",
+        title: "O Povo Brasileiro de Darcy Ribeiro - Matriz Afro (parte 3)",
+        url: "https://youtu.be/vwj1GBEYr_s?si=pKYjllprMlagd2dh",
         type: "youtube"
     },
 {
-        title: "Ensaios sobre a colonialidade do poder de Aníbal Quijano",
-        url: "https://youtu.be/sA1AQ-xYfys?si=LCrCfxSujBIm4qFw",
+        title: "Colonização das Filipinas e imperialismo americano no Sudeste Asiático",
+        url: "https://youtu.be/5yKO0mHhHXk?si=VIv61FK-ig098AMq",
         type: "youtube"
     },
 {
-        title: "O Complexo de Salvador Branco: A Bondade que Destrói",
+        title: "Amigo (2010)",
+        url: "https://youtu.be/42Gl7uoU7hc?si=4punW5qiVbu9_nJf",
+        type: "youtube"
+    },
+{
+        title: "O Complexo de Salvador Branco, A Bondade que Destrói",
         url: "https://youtu.be/lgod0RIGW5w?si=9YvkBmcfQuR1puDR",
+        type: "youtube"
+    },
+{
+        title: "O Massacre Alemão na Namíbia, O primeiro Genocídio do Século XX",
+        url: "https://youtu.be/-J5gJrW1epI?si=HBma-PmkoiZu9bRk",
+        type: "youtube"
+    },
+{
+        title: "A terrível história de atrocidades do domínio belga no Congo",
+        url: "https://youtu.be/FXgJiThAMl8?si=STth46ktsY20caSG",
+        type: "youtube"
+    },
+{
+        title: "O Genocídio Esquecido da Europa no Congo Belga",
+        url: "https://youtu.be/8R6Ysku72Po?si=6K94WOO0e6zSLBy2",
         type: "youtube"
     }
 ];
