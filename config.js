d07f8638c4a3fa6cbdd96b9b272bc534deea2e80
hemadacosta <156354202+hemadacosta@users.noreply.github.com>
2026-04-01 20:57:57 -0300

    Update video schedule with new entries and formats
    Signed-off-by: hemadacosta <156354202+hemadacosta@users.noreply.github.com>

--- config.js
+++ config.js
@@ -82,5 +82,11 @@ const schedule = [
     title: "Darcy Ribeiro",
     url: "https://player.vimeo.com/video/205497849?h=7871f98a5c",
     type: "vimeo"
-    }
+    },
+{
+    title: "A Roupa dos Brancos Mortos",
+    url: "https://geo.dailymotion.com/player.html?video=x8u8f44",
+    // ou: url: "https://dai.ly/x123abc"
+    type: "dailymotion"
+  }
 ];
