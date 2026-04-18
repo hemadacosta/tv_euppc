80e0117bf73de5129544b5f08449bf0bc9e400c7
hemadacosta <156354202+hemadacosta@users.noreply.github.com>
2026-04-18 10:40:42 -0300

    Update config.js
    Signed-off-by: hemadacosta <156354202+hemadacosta@users.noreply.github.com>

--- config.js
+++ config.js
@@ -4,128 +4,203 @@
 
 const schedule = [
     {
-        title: "9ª Aula - EUPPC - Hibridismos culturais: Garífunas",
+    title: "1ªAula - EUPPC - Apresentação da Disciplina EUPPC",
+    url: "https://youtu.be/Ol4YvwTu2tI?si=sGMBTiSbDu0N6CMG",
+    type: "youtube"
+    }, 
+    { 
+    title: "TV EUPPC",
+    url: "https://youtu.be/FQKYXbr5rHo?si=LG89ea4FYvbR0pn6",
+    type: "youtube"
+    },
+    {
+    title: "2ª Aula - EUPPC - Leitura inspecional e leitura averiguativa",
+    url: "https://youtu.be/AhX7j03dhxI?si=snqXwJ_b0MVc2PVM",
+    type: "youtube"
+    },
+    { 
+    title: "TV EUPPC",
+    url: "https://youtu.be/FQKYXbr5rHo?si=LG89ea4FYvbR0pn6",
+    type: "youtube"
+    },
+    {
+    title: "Schooling the World (Escolarizando o Mundo), o último fardo do homem branco",
+    url: "https://youtu.be/6t_HN95-Urs?si=J8XEwF_pH_51lqiB",
+    type: "youtube"
+    },
+    {
+    title: "3ª Videoaula - EUPPC - Por que devemos desinstalar a escola",
+    url: "https://youtu.be/_Jj7tUgrFbA?si=KxPDpfuTWYlcq_Ah",
+    type: "youtube"
+    },
+    { 
+    title: "TV EUPPC",
+    url: "https://youtu.be/FQKYXbr5rHo?si=LG89ea4FYvbR0pn6",
+    type: "youtube"
+    },
+    {
+    title: "4ª Videoaula - EUPPC - Introdução ao documentário Schooling the World",
+    url: "https://youtu.be/wgpRePgjJUA?si=4XmTh4XfNjgBlVj7",
+    type: "youtube"
+    },
+    {
+    title: "5ª Aula - EUPPC - Reflexões sobre o documentário Schooling the World",
+    url: "https://youtu.be/ocKkkbSAl4g?si=sDDTCXTo2wLSgJsM",
+    type: "youtube"
+    },
+    { 
+    title: "TV EUPPC",
+    url: "https://youtu.be/FQKYXbr5rHo?si=LG89ea4FYvbR0pn6",
+    type: "youtube"
+    },
+    {
+    title: "6ª Aula - EUPPC  - Diferenças no processo educacional colonial a partir da catequese operadas por diferentes ordens religiosas",
+    url: "https://youtu.be/2w0IWQP2WLA?si=AmluB6s5wr3bajN9",
+    type: "youtube"
+    },
+    {
+    title: '🎬 <a href="https://drive.google.com/file/d/1GbVr5RNUtdojCl2-Wnm8cov5zhBrNzmR/preview" target="_blank" style="color: #4CAF50; font-weight: bold;">Ver Filme Completo</a> Anna e o Rei (1999) - Trailer',
+    url: "https://youtu.be/I6cF4_502aw?si=NWE5szdtOKzpi10p",
+    type: "youtube"
+    },
+    {     
+    title: "TV EUPPC",
+    url: "https://youtu.be/FQKYXbr5rHo?si=LG89ea4FYvbR0pn6",
+    type: "youtube"
+    },
+    {
+    title: "Capitão Fantástico (2016)",
+    url: "https://youtu.be/hkzW4ft8P0k?si=-vM-lQuRdcvoH8tZ",
+    type: "youtube"
+    },
+    {
+    title: "7ª Aula - EUPPC - O Casamento entre ciência e império",
+    url: "https://youtu.be/6nsz0OzbsAQ",
+    type: "youtube"
+    },
+    {     
+    title: "TV EUPPC",
+    url: "https://youtu.be/FQKYXbr5rHo?si=LG89ea4FYvbR0pn6",
+    type: "youtube"
+    },
+    {
+    title: "8ª Aula - EUPPC  - Etnografia e Observação Participante, um método de pesquisa social originada na estrutura colonial imperialista",
+    url: "https://youtu.be/Xgj7YxQRwW4",
+    type: "youtube"
+    },
+    {
+    title: "O Homem que Viu o Infinito (The Man Who Knew Infinity, 2015)",
+    url: "https://youtu.be/3Istn6lAnJY?si=t0HnAwpbNa2EvcBT",
+    type: "youtube"
+    },
+    {     
+    title: "TV EUPPC",
+    url: "https://youtu.be/FQKYXbr5rHo?si=LG89ea4FYvbR0pn6",
+    type: "youtube"
+    },
+    {
+    title: '🎬 <a href="https://drive.google.com/file/d/1UIVtJrR_FNGcPvsXJwMgP12AtlZJArbV/preview" target="_blank" style="color: #4CAF50; font-weight: bold;">Ver Filme Completo</a> | A Sombra e a escuridão - Trailer',
+    url: "https://youtu.be/I6cF4_502aw?si=NWE5szdtOKzpi10p",
+    type: "youtube"
+    },
+    {
+    title: "Vênus Negra (2010)",
+    url: "https://youtu.be/3QdOKt4oEKo?si=JCj-mAleUm_HXs65",
+    type: "youtube"
+    },
+    {     
+    title: "TV EUPPC",
+    url: "https://youtu.be/FQKYXbr5rHo?si=LG89ea4FYvbR0pn6",
+    type: "youtube"
+    },
+    {
+    title: "La Negra (1966)",
+    url: "https://youtu.be/qbFJLxMGApE?si=ZRccWHPm-u6gv-_3",
+    type: "youtube"
+    },
+    {
+    title: "9ª Aula - EUPPC - Hibridismos culturais: Garífunas",
         url: "https://youtu.be/x07Vx6kEcmo",
         type: "youtube"
     },
     {     
-        title: "TV EUPPC",
-        url: "https://youtu.be/FQKYXbr5rHo?si=LG89ea4FYvbR0pn6",
-        type: "youtube"
-      },
-     {
-        title: "10ª Aula - EUPPC  - O Pós-colonial a partir de Stuart Hall, Ella Shohat e Chinua Achebe",
-        url: "https://youtu.be/IRV6wkq9kjw?si=uH9gG6ZoNUjnRgHY",
-        type: "youtube"
-    },
-{
-        tle: "TV EUPPC",
-        url: "https://youtu.be/FQKYXbr5rHo?si=LG89ea4FYvbR0pn6",
-        type: "youtube"
-   },
-{
-        title: "Introdução a Stuart Hall e as origens dos Estudos Culturais",
-        url: "https://youtu.be/51thL-texS8?si=Uu8shh72jk_EZXUt",
-        type: "youtube"
-    },
-     {     
-        title: "TV EUPPC",
-        url: "https://youtu.be/FQKYXbr5rHo?si=LG89ea4FYvbR0pn6",
-        type: "youtube"
-      },
-{
-        title: "Professora Ella Shohat, Naturalização e animalização",
-        url: "https://youtu.be/jlCw9ErG29c?si=qLY0fsfoYBgvzE11",
-        type: "youtube"
-    },
- {     
-        title: "TV EUPPC",
-        url: "https://youtu.be/FQKYXbr5rHo?si=LG89ea4FYvbR0pn6",
-        type: "youtube"
-      },
-{
-        title: "Chinua Achebe discursa sobre por que a África",
-        url: "https://youtu.be/EYgl0CH4zH8?si=9mq_IleBfWZp2klI",
-        type: "youtube"
-    },
-{
-        title: "O mundo se despedaça, de Chinua Achebe | por Ana Lis Soares",
-        url: "https://youtu.be/J-1IhuzNfYI?si=dit1wk9MKzWnWzbi",
-        type: "youtube"
-    },
-{
-        title: "TV EUPPC",
-        url: "https://youtu.be/FQKYXbr5rHo?si=LG89ea4FYvbR0pn6",
-        type: "youtube"
-    },
-{
-        title: "1492, A conquista do Paraíso (1992)",
-        url: "https://youtu.be/KSv7zlSIBVc?si=XvZaEaXwiEIp-BLU",
-        type: "youtube"
-    },
-{     
-        title: "TV EUPPC",
-        url: "https://youtu.be/FQKYXbr5rHo?si=LG89ea4FYvbR0pn6",
-        type: "youtube"
-      },
-    {
-    title: '🎬 <a href="https://drive.google.com/file/d/1nosjwc8YC_J5QQAO2fkXxYjfhWaZarm9/preview" target="_blank" style="color: #4CAF50; font-weight: bold;">Ver Filme Completo</a> Diamante de Sangue (2006) - Trailer', //2:17
-    url: "https://youtu.be/5wHxPWRlkUg?si=pJFOIZvfj6-e64Kq",
+    title: "TV EUPPC",
+    url: "https://youtu.be/FQKYXbr5rHo?si=LG89ea4FYvbR0pn6",
     type: "youtube"
     },
     {
-        title: "Diálogos - Desafios para a decolonialidade",
-        url: "https://youtu.be/qFZki_sr6ws?si=fYR7cqk7bb7QRrIo",
-        type: "youtube"
+    title: "10ª Aula - EUPPC  - O Pós-colonial a partir de Stuart Hall, Ella Shohat e Chinua Achebe", //1:03:10
+    url: "https://youtu.be/IRV6wkq9kjw?si=uH9gG6ZoNUjnRgHY",
+    type: "youtube"
     }, 
-{
-        title: "O Povo Brasileiro Darcy Ribeiro - O Brasil Crioulo - parte 5",
-        url: "https://youtu.be/oObneYXQedk?si=JmNyujDAnZWTzIoA",
-        type: "youtube"
-    },
-{
-        title: "María Lugones e a crítica ao pensamento colonial",
-        url: "https://youtu.be/-hjThulgEPI?si=AeO9mveyFSB5L3ax",
-        type: "youtube"
-    },
-{
-       title: "Mulherismo Africana",
-        url: "https://youtu.be/rYjbAC_ij_Q?si=h2l8LY2khRfw5FQq",
-        type: "youtube"
+    {
+    title: 'A Missão (1986) - Trailer | <a href="https://drive.google.com/file/d/1YunkakApLq5eDzp1eJL3GsWl-iPbHlog/preview" target="_blank">🎬 Assistir Filme</a>',
+    url: "https://youtu.be/o7aTvYJBHw4?si=_8gJ73ztoctemmgj",
+    type: "youtube"
     },
     {     
-        title: "TV EUPPC",
-        url: "https://youtu.be/FQKYXbr5rHo?si=LG89ea4FYvbR0pn6",
-        type: "youtube"
-      },
-{
-        title: "Independência (2015)",
-        url: "https://player.vimeo.com/video/337573703?h=88c00d5c50",
-        type: "vimeo"
-    },
-{
-        title: "La Negra (1966)",
-        url: "https://youtu.be/qbFJLxMGApE?si=ZRccWHPm-u6gv-_3",
-        type: "youtube"
-    },
-        {
-        title: "Una verdadera Historia sobre Nosotros los Garifunas",
-        url: "https://youtu.be/XnfII70hVzg?si=_C1qt6lqdwM9F8rJ",
-        type: "youtube"
-    },
-{
-        title: "Aurelio - Lándini (teaser clip)",
-        url: "https://youtu.be/_KgzUkucqII?si=YJL6Rj2etIWYe_Rx",
-        type: "youtube"
+    title: "TV EUPPC",
+    url: "https://youtu.be/FQKYXbr5rHo?si=LG89ea4FYvbR0pn6",
+    type: "youtube"
     },
     {
-    title: "Yurumein | La historia Garífuna de resistencia y herencia cultural en Guatemala",
-        url: "https://youtu.be/ayA09Vb6438?si=wjy9tV8LmFrgrkUZ",
-        type: "youtube"
+    title: "1492: A conquista do Paraíso",
+    url: "https://youtu.be/KSv7zlSIBVc?si=XvZaEaXwiEIp-BLU",
+    type: "youtube"
+    },
+    {
+    title: '🎬 <a href="https://drive.google.com/file/d/1nosjwc8YC_J5QQAO2fkXxYjfhWaZarm9/preview" target="_blank" style="color: #4CAF50; font-weight: bold;">Ver Filme Completo</a> Diamante de Sangue (2006) - Trailer',
+    url: "https://youtu.be/5wHxPWRlkUg?si=pJFOIZvfj6-e64Kq",
+    type: "youtube"
+    },
+    {     
+    title: "TV EUPPC",
+    url: "https://youtu.be/FQKYXbr5rHo?si=LG89ea4FYvbR0pn6",
+    type: "youtube"
     },
     {
     title: '🎬 <a href="https://drive.google.com/file/d/1MioKnMk7mH6Mqla0b0rRRFW7ac6yCbI5/preview" target="_blank" style="color: #4CAF50; font-weight: bold;">Ver Filme Completo</a> Pantera Negra (2018) - Trailer',
     url: "https://youtu.be/_Z3QKkl1WyM?si=hZctbT8qsvh65C3G",
     type: "youtube"
-}
-];
+    },
+    {
+    title: "O Povo Brasileiro Darcy Ribeiro - Matriz Tupi - parte 1",
+    url: "https://youtu.be/rQOPdiEdX24?si=cgbh3kTfFSkR6V9D",
+    type: "youtube"
+    },
+    {     
+    title: "TV EUPPC",
+    url: "https://youtu.be/FQKYXbr5rHo?si=LG89ea4FYvbR0pn6",
+    type: "youtube"
+    },
+    {
+    title: "O Povo Brasileiro Darcy Ribeiro - Matriz Lusa - parte 2",
+    url: "https://youtu.be/ZxOCLwMQ_ik?si=gxtfUoiCSJ0nchOY",
+    type: "youtube"
+    },
+    {
+    title: "O Povo Brasileiro Darcy Ribeiro - Matriz Afro - parte 3",
+    url: "https://youtu.be/vwj1GBEYr_s?si=v2dsjnfUrK1nMz2_",
+    type: "youtube"
+    },
+    {     
+    title: "TV EUPPC",
+    url: "https://youtu.be/FQKYXbr5rHo?si=LG89ea4FYvbR0pn6",
+    type: "youtube"
+    },
+    {
+    title: "O Povo Brasileiro Darcy Ribeiro - Encontros e Desencontros - parte 4",
+    url: "https://youtu.be/Dl77HvU2fUk?si=DvcXRKglW8-Yke8w",
+    type: "youtube"
+    },
+    {
+    title: "O Povo Brasileiro Darcy Ribeiro - O Brasil Crioulo - parte 5", //26:03
+    url: "https://youtu.be/oObneYXQedk?si=JmNyujDAnZWTzIoA",
+    type: "youtube"
+    },
+    {     
+    title: "TV EUPPC",
+    url: "https://youtu.be/FQKYXbr5rHo?si=LG89ea4FYvbR0pn6",
+    type: "youtube"
+    }
+    ];
